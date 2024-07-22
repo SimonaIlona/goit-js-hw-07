2160423deac422203a76e68406bc8120d4d964de
SimonaIlona <simona.anghelinis19@gmail.com>
2024-07-22 19:07:32 +0300

    final commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,31 @@
+import { galleryItems } from "./gallery-items.js";
+
+console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+	const listItem = document.createElement("li");
+	listItem.classList.add("gallery__item");
+	listItem.innerHTML = `
+  <a class='gallery__link' href='${item.original}'>
+    <img class="gallery__image"
+    src='${item.preview}'
+    data-source='${item.original}'
+    alt='${item.description}' />
+  </a>
+  `;
+	listEl.append(listItem);
+});
+
+listEl.addEventListener("click", openImageInModal);
+
+function openImageInModal(event) {
+	event.preventDefault();
+
+	const lightBox = new SimpleLightbox(".gallery a", {
+		captionPosition: "bottom",
+		captionsData: "alt",
+		captionDelay: 250,
+	});
+}
